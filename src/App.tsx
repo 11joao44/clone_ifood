3d196619c5a8f10773022884da985395f17d50ce
João Pedro <11joao44@gmail.com>
2023-05-23 15:37:07 -0400

    commit initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <div>
+      <Header>Ola mundo</Header>
+    </div>
+  )
+}
+
+export default App
+
+const Header = styled.header`
+  background: red;
+  font-size: 50px;
+`
